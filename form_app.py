8b5cc758e608663d13f28c57e68c6100c3e98825
Yqmaoka Tetsuji <sirokuma3919@gmail.com>
2026-08-20 20:11:26 +0900

    Update form_app.py

--- form_app.py
+++ form_app.py
@@ -26,7 +26,7 @@ LOCK_FILE = f'{TARGET_MONTH}月シフト提出状況.lock'
 DEPARTMENTS = ['選択してください', '季節AV', '家電', '情報', '通信']
 
 # パスワードの指定
-ADMIN_PASSWORD = st.secrets['ademin_password']
+ADMIN_PASSWORD = st.secrets['admin_password']
 
 # GoogleスプレッドシートのScriptsたち
 GAS_URL = st.secrets['gas_url']
